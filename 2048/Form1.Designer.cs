c0b5329db05236eb80fa0dc882b386fbad5b6974
s221363 <s221363@M4-GC11988>
2023-12-19 13:54:40 +0000

    Add project files.

---
+++ 2048/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace _2048
+{
+    partial class Root
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Root));
+            SuspendLayout();
+            // 
+            // Root
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(499, 497);
+            MaximumSize = new Size(515, 536);
+            MinimumSize = new Size(502, 500);
+            Name = "Root";
+            Text = "2048";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            PreviewKeyDown += Form1_PreviewKeyDown;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ 2048/Form1.cs
@@ -0,0 +1,100 @@
+namespace _2048
+{
+    public partial class Root : Form
+    {
+        int[,]? data = null;
+        int[,]? buttons = null;
+        Dictionary<int, string> ImageNames = new Dictionary<int, string>
+        {
+            {2, "two"},
+            {4, "four"},
+            {8, "eight"},
+            {16, "sixteen"},
+            {32, "thirtytwo"},
+            {64, "sixtyfour"},
+            {128, "onehundredtwentyeight"},
+            {256, "twohundredfiftysix"},
+            {512, "fivehundredtwelve"},
+            {1024, "onethousandtwentyfour"}
+        };
+        public Root()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.data = new int[,] { { 2, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            CreateButtonDelegate();
+        }
+        private void CreateButtonDelegate()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j=0; j < 4; j++)
+                {
+                    PictureBox newBox = new()
+                    {
+                        Name = i.ToString() + j.ToString(),
+                        Text = "Created Box",
+                        Location = new Point(i * 120 +13, j * 120 +13),
+                        Size = new Size(112, 112)
+                    };
+                    this.Controls.Add(newBox);
+                    newBox.BringToFront();
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int[] Arrows = { 37, 38, 39, 40 };
+            if (!Arrows.Contains(e.KeyValue)) { return; }
+            if (this.data!=null) { MessageBox.Show(this.data[1,1].ToString()); }
+            switch (e.KeyValue)
+            {
+                case (37):
+                    //Left arrow
+                    break;
+                case (38):
+                    //Up arrow
+                    break;
+                case (39):
+                    //Right arrow
+                    break;
+                case (40):
+                    //Down arrow
+                    break;
+                default:
+                    //Literally shouldn't happen
+                    break;
+            }
+            UpdateGrid();
+        }
+        private void UpdateGrid()
+        {
+            //This function should access the multidimensional array and then use this to update the image widgets
+            for (int i=0;i<4;i++)
+            {
+                for (int j=0;j<4;j++) 
+                {
+                    
+                }
+            }
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            return;
+            /*switch (e.KeyCode)
+            {
+                case Keys.Down:
+                case Keys.Up:
+                    //e.IsInputKey = true;
+                    break;
+            }*/
+            //Seems to work on new versions of .NET without needing this
+        }
+    }
+}
